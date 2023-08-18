bdc084734fcae823d9ef407892bd46253a47a55e
genieouz <33516121+genieouz@users.noreply.github.com>
2023-08-18 18:11:12 +0000

    socket

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+const taskModel = require('./modules/tasks/task.model');
 
 // REALTIME SOCKET IO
 const socketIo = require('socket.io');
+const http = require('http');
+const server = http.createServer(app);
+const io = socketIo(server);
 
 mongoose.connect("mongodb+srv://edacytalent:12345@cluster0.9z1a3.mongodb.net/?retryWrites=true&w=majority")
 .then(() => {
@@ -24,25 +28,42 @@ mongoose.connect("mongodb+srv://edacytalent:12345@cluster0.9z1a3.mongodb.net/?re
         console.error(error);
     }
 )
-
 async function initApp() {
-
     app.get('/taches', async (req, res) => {
-        const status = req.query.status;
-        console.log({status})
+        const status = req.query.status || '';
         const user = { firstName: "John", lastName: "Bishop" }
         const taskList = await taskModel.find({ status: { $regex: status, $options: "i" } });
         // const taskList = await taskModel.find({ status: status })
+        // res.json({taskList})
         res.status(200).render('index', { user, taches: taskList });
     })
 
+
+
+
+    io.on('connection', (socket) => {
+        console.log('Nouveau client connecté');
+    
+        socket.on('addItem', async (item) => {
+            const task = await taskModel.create(item)
+            io.emit('newItem', item);
+        });
+    
+        socket.on('disconnect', () => {
+            console.log('Client déconnecté');
+        });
+    });
+
     require('./modules/auth/auth.routes')(app);
-    require('./modules/auth/auth.guard')(app);
+    // require('./modules/auth/auth.guard')(app);
     require('./modules/tasks/task.routes')(app);
     
 }
 
 
-app.listen(3000, () => {
-    console.log("App is listening on port 3000");
-});
+// app.listen(3000, () => {
+//     console.log("App is listening on port 3000");
+// });
+server.listen(3000, () => {
+    console.log('Serveur en écoute sur le port 3000');
+  });
